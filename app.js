5a90957c372d622fadb2275c42024c1c0d2ce85e
SYC1205 <syc1205@gmail.com>
2016-04-01 14:43:13 +0800

    Rename index.js to app.js, let config to config.js

---
+++ app.js
@@ -0,0 +1,68 @@
+var colors = require('colors'),
+    AWS = require('aws-sdk'),
+    config = require('./config')
+
+var sqsQueueUrl,
+    testCredentialsParams,
+    sqs
+
+//Initial ........
+AWS.config.update(config.getRegion())      //sample: {region: 'ap-northeast-1'}
+testCredentialsParams = {QueueNamePrefix: 'docapi'}
+sqs = new AWS.SQS()                        // Instantiate SQS client
+
+var _this = module.exports = {
+    readMessage : function(){
+        //console.log(colors.cyan(config.getSQSParameter().QueueUrl));
+        sqs.receiveMessage(config.getSQSParameter(), function(err, data) {
+            if(!err){
+                // If there are any messages to get
+                if (data.Messages) {
+                    //console.log(data);
+                    // Get all messages (should be the only one since we said to only get one above)
+                    for (var msgIndex in data.Messages){
+                        var msg = data.Messages[msgIndex].Body
+                        console.log(msg)
+
+                        // Now this is where you'd do something with this message
+                        //doSomethingCool(body, message);  // whatever you wanna do
+
+                        // Clean up after yourself... delete this message from the queue, so it's not executed again
+                        //_this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
+                    } 
+                    _this.readMessage()    // Recursive ~~~~
+                }else{
+                    console.log(colors.cyan('wait for queue'))
+                    _this.readMessage(); // If no data, recursive ~
+                }
+            }else{
+                //Need send email to alert someone
+                console.log(colors.red(err))
+            }
+        })
+    },
+
+    removeFromQueue : function(ReceiptHandle) {
+        sqs.deleteMessage({
+                QueueUrl: config.getSQSParameter().QueueUrl,
+                ReceiptHandle: ReceiptHandle, 
+            }, function(err, data) {
+                if(err) console.log(err, err.stack);    // an error occurred
+                else console.log(data)                  // successful response
+            })
+    }
+}
+
+//This is for self test credentials, use EC2 role or local file.
+sqs.listQueues(testCredentialsParams, function(err, data) {
+    if(err){ 
+        console.log(colors.red(err.message)) // an error occurred
+        console.log(colors.cyan('Get local credentials' + config.getCredentialsPath()))
+        AWS.config.loadFromPath(config.getCredentialsPath()) // Load credentials from local json file
+        sqs = new AWS.SQS()    //Reset instantiate
+        _this.readMessage()    //Recursive go~
+    }else{
+        console.log(colors.cyan('EC2 credentials'))
+        _this.readMessage()   //Recursive go~
+    }     
+})
--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 module.exports = {
-	getSQSParameter : function(){
-		var sqsQueueUrl = 'https://sqs.ap-northeast-1.amazonaws.com/838559313065/docapi-jobs-linux-hi';
-		return sqsQueueUrl;		
+	getSQSParameter :  function(){
+		var sqsParameter = {
+                QueueUrl: 'https://sqs.ap-northeast-1.amazonaws.com/838559313065/docapi-jobs-linux-hi',
+                MaxNumberOfMessages: 1,     // how many messages do we wanna retrieve?
+                VisibilityTimeout: 30,      // seconds - how long we want a lock on this job
+                WaitTimeSeconds: 20         // seconds - how long should we wait for a message?
+            }
+        return sqsParameter;	
 	},
 	getCredentialsPath: function(){
 		var credentialsPath = './aws.credentials.json';
 		return credentialsPath;		
+	},
+	getRegion: function(){
+		var region = {region: 'ap-northeast-1'};
+		return region;		
 	},
 };
