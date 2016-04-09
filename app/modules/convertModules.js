c5176488fdf99bf7fa882a6114f26fcb0d38c5be
SYC1205 <syc1205@gmail.com>
2016-04-10 00:35:37 +0800

    fix something

--- app/modules/convertModules.js
+++ app/modules/convertModules.js
@@ -11,32 +11,27 @@ var _this = module.exports = {
 				var fileName = fileFullName.substring(0, fileFullName.indexOf('.'))
 				    fileNameExt = fileFullName.substring(fileFullName.indexOf('.')+1, fileFullName.length)
 
-				if(!targetArray[index].toTypeObj){
-					var options = {
-						src: dir+'/'+fileFullName,
-						dst: dir+'/'+fileName+'_'+targetArray[index].tag+'.'+fileNameExt,
-						width: targetArray[index].width,
-						height: imginfo.height / imginfo.width * targetArray[index].width
-					}
-
-					easy.resize(options).then(
-						function (file) {
-							console.log('convert OK : ' + options.dst)
-							if (index < targetArray.length - 1){
-								_this.resizeImg(fileFullName, targetArray, index + 1, callback)  // Recursive ~~~~
-							}else{
-								callback();
-							}
-						}, function (err) {
-							console.log(err);
-							callback(err);
-							return;
-						}
-					);
-				}else{
-					console.log('err');
-					callback();
+				var options = {
+					src: dir+'/'+fileFullName,
+					dst: dir+'/'+fileName+'_'+targetArray[index].tag+'.'+fileNameExt,
+					width: targetArray[index].width,
+					height: imginfo.height / imginfo.width * targetArray[index].width
 				}
+
+				easy.resize(options).then(
+					function (file) {
+						console.log('convert OK : ' + options.dst)
+						if (index < targetArray.length - 1){
+							_this.resizeImg(fileFullName, targetArray, index + 1, callback)  // Recursive ~~~~
+						}else{
+							callback()
+						}
+					}, function (err) {
+						console.log(err);
+						callback(err);
+						return
+					}
+				)
 
 			},function (err) {
 				console.log(err)
