7b52414bf5a24117561bac922689b7d3d83ba0d0
SYC1205 <syc1205@gmail.com>
2016-04-26 14:18:54 +0800

    Control sqs data is null

--- app/app.js
+++ app/app.js
@@ -6,58 +6,55 @@ var colors = require('colors'),
     config = require('./config'),
     convertFlow = require('./modules/convertFlow')
 
-var sqsQueueUrl,
+var sqsQueueUrl = config.getSQSParameter(),
     sqs
 
 var _this = module.exports = {
     readMessage : function(){
-        sqs.receiveMessage(config.getSQSParameter(), function(err, data) {
-            if(!err){
-                // If there are any messages to get
-                if (data.Messages) {
-                    //console.log(data);
-                    // Get all messages (should be the only one since we said to only get one above)
-                    for (var msgIndex in data.Messages){
-                        // Now this is where you'd do something with this message
-                        var msg = JSON.parse(data.Messages[msgIndex].Body)
-                        console.log(data.Messages)
-                        if(msg.filePath){
-                            var mediaType = msg.filePath.substring(msg.filePath.indexOf('.')+1 , msg.filePath.length).toLowerCase()
-                            switch (mediaType){
-                                case 'jpg':
-                                case 'png':
-                                case 'gif':
-                                    if(!msg.convertItems[0].body){
-                                        console.log(colors.red("sqs formate error"))
-                                        console.log(colors.cyan('wait for queue'))
-                                        _this.readMessage()    // Recursive ~~~~
-                                    
-                                    }else{
+        sqs.receiveMessage(sqsQueueUrl, function(err, data) {
+            if(err) {   //Need send email to alert someone
+                console.log(colors.red(err))
+                return
+            }
 
-                                        convertFlow.imgFlow(msg.filePath, msg.convertItems[0].body.extra.multiAction, function(err, result) {
-                                            if(err){  // an error occurred
-                                                console.log(err, err.stack)
-                                            } else{
-                                                console.log(colors.cyan('Job done!'))   // successful response
+            // If there are any messages to get
+            if (data.Messages) {
+                //console.log(data);
+                // Get all messages (should be the only one since we said to only get one above)
+                for (var msgIndex in data.Messages){
+                    // Now this is where you'd do something with this message
+                    var msg = data.Messages[msgIndex].Body
+                    
+                    if(msg==''||msg=='{}'||msg=='[]'){
+                        console.log(colors.red("sqs data is null"))
+                        //Clean up after yourself... delete this message from the queue, so it's not executed again
+                        _this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
+                        _this.readMessage()
+                        return
+                    }else{
+                        msg = JSON.parse(msg)
+                    }
 
-                                                //Clean up after yourself... delete this message from the queue, so it's not executed again
-                                                _this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
-                                            }
+                    
+                    if(msg.filePath){
+                        var mediaType = msg.filePath.substring(msg.filePath.indexOf('.')+1 , msg.filePath.length).toLowerCase()
+                        switch (mediaType){
+                            case 'jpg':
+                            case 'png':
+                            case 'gif':
+                                if(!msg.convertItems[0].body){
+                                    console.log(colors.red("sqs formate no body"))
+                                    console.log(colors.cyan('wait for queue'))
+                                    _this.readMessage()    // Recursive ~~~~
+                                
+                                }else{
 
-                                            console.log(colors.cyan('wait for queue'))
-                                            _this.readMessage()    // Recursive ~~~~
-                                        }) 
-
-                                    }
-                                    break
-                                case 'avi':
-                                case 'wmv':
-                                case 'mov':
-                                    convertFlow.mediaFlow(msg.filePath, null, function(err, result) {
+                                    convertFlow.imgFlow(msg.filePath, msg.convertItems[0].body.extra.multiAction, function(err, result) {
                                         if(err){  // an error occurred
                                             console.log(err, err.stack)
                                         } else{
                                             console.log(colors.cyan('Job done!'))   // successful response
+
                                             //Clean up after yourself... delete this message from the queue, so it's not executed again
                                             _this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
                                         }
@@ -65,45 +62,62 @@ var _this = module.exports = {
                                         console.log(colors.cyan('wait for queue'))
                                         _this.readMessage()    // Recursive ~~~~
                                     }) 
-                                    break
-                                case 'mp3':
-                                    convertFlow.audioFlow()
-                                    break
-                                default:
-                                    console.log(colors.red("unsupport media Type"))
+
+                                }
+                                break
+                            case 'avi':
+                            case 'wmv':
+                            case 'mov':
+                                convertFlow.mediaFlow(msg.filePath, null, function(err, result) {
+                                    if(err){  // an error occurred
+                                        console.log(err, err.stack)
+                                    } else{
+                                        console.log(colors.cyan('Job done!'))   // successful response
+                                        //Clean up after yourself... delete this message from the queue, so it's not executed again
+                                        _this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
+                                    }
+
                                     console.log(colors.cyan('wait for queue'))
                                     _this.readMessage()    // Recursive ~~~~
-                            }
-                        }else{
-                            // maybe send something to another queue
-                            console.log(colors.red("sqs formate error"))
-                            console.log(colors.cyan('wait for queue'))
-                            _this.readMessage()    // Recursive ~~~~
+                                }) 
+                                break
+                            case 'mp3':
+                                convertFlow.audioFlow()
+                                break
+                            default:
+                                console.log(colors.red("unsupport media Type"))
+                                console.log(colors.cyan('wait for queue'))
+                                _this.readMessage()    // Recursive ~~~~
                         }
-                        // Clean up after yourself... delete this message from the queue, so it's not executed again
-                        //_this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
-                    }
+                    }else{
+                        console.log(data.Messages)
 
-                   
-                }else{
-                    console.log(colors.cyan('wait for queue'))
-                    _this.readMessage(); // If no data, recursive ~
+                        // maybe send something to another queue
+                        console.log(colors.red("sqs formate no filePath"))
+                        console.log(colors.cyan('wait for queue'))
+                        _this.readMessage()    // Recursive ~~~~
+                    }
+                    // Clean up after yourself... delete this message from the queue, so it's not executed again
+                    //_this.removeFromQueue(data.Messages[msgIndex].ReceiptHandle);  // We'll do this in a second
                 }
+
+               
             }else{
-                //Need send email to alert someone
-                console.log(colors.red(err))
+                console.log(colors.cyan('wait for queue'))
+                _this.readMessage(); // If no data, recursive ~
             }
+
         })
     },
 
     removeFromQueue : function(ReceiptHandle) {
         //console.log(ReceiptHandle)
         sqs.deleteMessage({
-                QueueUrl: config.getSQSParameter().QueueUrl,
+                QueueUrl: sqsQueueUrl.QueueUrl,
                 ReceiptHandle: ReceiptHandle, 
             }, function(err, data) {
                 if(err) console.log(err, err.stack);    // an error occurred
-                else console.log(data)                  // successful response
+                //else console.log(data)                  // successful response
             })
     }
 }
